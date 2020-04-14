4d9ec839f58c6381217c1324fb60beffa2864649
FoxyChmoxy <almasgaara@mail.ru>
2020-04-14 18:38:35 +0600

    RDSController: HttpCLient and AutoMapper

---
+++ UserInterface/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Common;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace UserInterface.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public UserController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = httpClientFactory.CreateClient();
+            var url = ApiDestinations.MAIN_CONTAINER + "user";
+            var result = await client.GetStringAsync(url);
+            var users = JsonConvert.DeserializeObject<List<User>>(result);
+            return View(users);
+        }
+    }
+}
