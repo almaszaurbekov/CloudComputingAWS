c2ef5e708c571a486cfe223f114bee1877170ae2
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-04-17 04:55:29 +0600

    Added AutoMapper

---
+++ UserInterface/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccess.JsonModels;
+using UserInterface.Models;
+namespace UserInterface.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserJsonModel, UserViewModel>(MemberList.Source);
+            CreateMap<UserJsonModel, UserViewModel>(MemberList.None);
+        }
+    }
+}
