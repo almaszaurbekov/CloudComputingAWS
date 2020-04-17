8999f6f8be9ca9825f5733c1cbc44ce728d7b310
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-04-18 02:52:15 +0600

    ContainerMDB: Added Order Service

---
+++ DataAccess/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using DataAccess.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Services
+{
+    public class OrderService
+    {
+        private readonly IMongoCollection<Order> orders;
+
+        public OrderService()
+        {
+            var client = new MongoClient(ApiDestinations.MONGO_CLIENT);
+            var database = client.GetDatabase(ApiDestinations.MONGO_DATABASE);
+            orders = database.GetCollection<Order>("Orders");
+        }
+
+        public List<Order> Get() =>
+            orders.Find(order => true).ToList();
+    }
+}
