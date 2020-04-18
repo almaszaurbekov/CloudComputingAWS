a4ff6eaa6bf22db3fa8267b9252e14cdcadcdb1f
Almas Zaurbekov <foxxychmoxy@gmail.com>
2020-04-19 03:39:39 +0600

    ContainerMDB: Added Post Entity

--- DataAccess/Models/Post.cs
+++ DataAccess/Models/Post.cs
@@ -7,5 +7,9 @@ namespace DataAccess.Models
 {
     public class Post : RdsModel
     {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
     }
 }
